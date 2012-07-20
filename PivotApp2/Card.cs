fc058dce094ad729fa03246e7ae0c0c665e26b8e
psi-gh <mitis74@yandex.ru>
2012-07-20 13:55:23 +0500

    test commit

--- PivotApp2/Card.cs
+++ PivotApp2/Card.cs
@@ -8,7 +8,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
-
+//test
 namespace PokerTools
 {
     public enum Suits { spades, hearts, diamonds, clubs };
